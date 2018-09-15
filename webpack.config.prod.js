e103eed6fad9f81b928e77ebc18150b92fc5b0f5
luvuong-le <lu-vuongle@hotmail.com>
2018-09-15 19:12:16 +1000

    [Lu-Vuong Le] Updated webpack config files and dist files

---
+++ webpack.config.prod.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    entry: ['./src/js/mplayer.js'],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/js'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/mplayer.min.css',
+        }),
+        new OptimizeCSSAssetsPlugin({}),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 8000,
+            files: ['./*/**'],
+            server: {
+                baseDir: ['./dist/'],
+            },
+        }),
+    ],
+};
